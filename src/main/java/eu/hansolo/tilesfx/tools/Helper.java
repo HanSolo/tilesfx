593304ae91ab852ec4aa284c6d1500b2dc68bbc2
Gerrit Grunwald <han.solo@mac.com>
2018-04-21 16:42:47 +0200

    Minor cosmetics

--- src/main/java/eu/hansolo/tilesfx/tools/Helper.java
+++ src/main/java/eu/hansolo/tilesfx/tools/Helper.java
@@ -257,11 +257,13 @@ public class Helper {
             TEXT.setFont(new Font(FONT_NAME, adjustableFontSize));
         }
     }
-    public static final void adjustTextSize(final Label TEXT, final double MAX_WIDTH, double fontSize) {
-        final String FONT_NAME = TEXT.getFont().getName();
-        while (TEXT.getBoundsInLocal().getWidth() > MAX_WIDTH && fontSize > 0) {
-            fontSize -= 0.05;
-            TEXT.setFont(new Font(FONT_NAME, fontSize));
+    public static final void adjustTextSize(final Label TEXT, final double MAX_WIDTH, final double FONT_SIZE) {
+        final String FONT_NAME          = TEXT.getFont().getName();
+        double       adjustableFontSize = FONT_SIZE;
+
+        while (TEXT.getBoundsInLocal().getWidth() > MAX_WIDTH && adjustableFontSize > 0) {
+            adjustableFontSize -= 0.05;
+            TEXT.setFont(new Font(FONT_NAME, adjustableFontSize));
         }
     }
 

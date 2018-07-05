4d15c3ab15229d080b0a3e2a044eb9b291093d73
Gerrit Grunwald <han.solo@mac.com>
2018-07-05 11:39:28 +0200

    Added 2 new color schemes

---
+++ src/main/java/eu/hansolo/tilesfx/colors/Dark.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.tilesfx.colors;
+
+import javafx.scene.paint.Color;
+
+
+/**
+ * Created by hansolo on 05.07.18.
+ */
+public class Dark implements TileColor {
+    public static Color RED           = Color.web("#b21c1a");
+    public static Color ORANGE_RED    = Color.web("#b2571a");
+    public static Color ORANGE        = Color.web("#b2751a");
+    public static Color YELLOW_ORANGE = Color.web("#b2941a");
+    public static Color YELLOW        = Color.web("#b1b11b");
+    public static Color GREEN_YELLOW  = Color.web("#739522");
+    public static Color GREEN         = Color.web("#267d2c");
+    public static Color BLUE_GREEN    = Color.web("#26667d");
+    public static Color BLUE          = Color.web("#26477d");
+    public static Color PURPLE_BLUE   = Color.web("#36267d");
+    public static Color PURPLE        = Color.web("#65267d");
+    public static Color RED_PURPLE    = Color.web("#8a245a");
+}
---
+++ src/main/java/eu/hansolo/tilesfx/colors/Medium.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.tilesfx.colors;
+
+import javafx.scene.paint.Color;
+
+
+/**
+ * Created by hansolo on 05.07.18.
+ */
+public class Medium implements TileColor {
+    public static Color RED           = Color.web("#df2420");
+    public static Color ORANGE_RED    = Color.web("#df6c20");
+    public static Color ORANGE        = Color.web("#df9320");
+    public static Color YELLOW_ORANGE = Color.web("#dfb920");
+    public static Color YELLOW        = Color.web("#dede21");
+    public static Color GREEN_YELLOW  = Color.web("#93bf2c");
+    public static Color GREEN         = Color.web("#32a539");
+    public static Color BLUE_GREEN    = Color.web("#3286a5");
+    public static Color BLUE          = Color.web("#325ea5");
+    public static Color PURPLE_BLUE   = Color.web("#4732a5");
+    public static Color PURPLE        = Color.web("#8432a5");
+    public static Color RED_PURPLE    = Color.web("#b22e75");
+}
+

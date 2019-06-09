47d02e0fec379970e4d42eacbee260b3631a58c6
Divine Threepwood <divine@openbase.org>
2019-06-09 21:10:52 +0200

    Implement workaround for issue HanSolo/tilesfx#73

---
+++ src/main/java/eu/hansolo/tilesfx/DemoLauncher.java
@@ -0,0 +1,22 @@
+package eu.hansolo.tilesfx;
+
+/**
+ * User: divine threepwood
+ * Date: 09.06.19
+ * Time: 21:00
+ */
+public class DemoLauncher {
+
+    /**
+     *
+     * This launcher starts the tilesfx demo pane.
+     *
+     * Note: This launcher is required to make sure all openjfx java modules are linked via the classpath.
+     * Please checkout issue https://github.com/HanSolo/tilesfx/issues/73 for more details.
+     *
+     * @param args application arguments
+     */
+    public static void main(String[] args) {
+        Demo.main(args);
+    }
+}

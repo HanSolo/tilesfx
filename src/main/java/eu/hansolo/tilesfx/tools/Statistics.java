1ed4c0b8c6fe4f64a8bf6583366d01d02286a1e6
Gerrit Grunwald <han.solo@mac.com>
2019-08-30 09:42:49 +0200

    Added methods to directly use ChartData objects in Statistics

--- src/main/java/eu/hansolo/tilesfx/tools/Statistics.java
+++ src/main/java/eu/hansolo/tilesfx/tools/Statistics.java
@@ -16,8 +16,11 @@
 
 package eu.hansolo.tilesfx.tools;
 
+import eu.hansolo.tilesfx.chart.ChartData;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -27,8 +30,14 @@ public class Statistics {
 
 
     // ******************** Methods *******************************************
+    public static final double getChartDataMean(final List<ChartData> DATA) {
+        return getMean(DATA.stream().map(ChartData::getValue).collect(Collectors.toList()));
+    }
     public static final double getMean(final List<Double> DATA) { return DATA.stream().mapToDouble(v -> v).sum() / DATA.size(); }
 
+    public static final double getChartDataVariance(final List<ChartData> DATA) {
+        return getVariance(DATA.stream().map(ChartData::getValue).collect(Collectors.toList()));
+    }
     public static final double getVariance(final List<Double> DATA) {
         double mean = getMean(DATA);
         double temp = 0;
@@ -36,15 +45,27 @@ public class Statistics {
         return temp / DATA.size();
     }
 
+    public static final double getChartDataStdDev(final List<ChartData> DATA) {
+        return getStdDev(DATA.stream().map(ChartData::getValue).collect(Collectors.toList()));
+    }
     public static final double getStdDev(final List<Double> DATA) { return Math.sqrt(getVariance(DATA)); }
 
+    public static final double getChartDataMedian(final List<ChartData> DATA) {
+        return getMedian(DATA.stream().map(ChartData::getValue).collect(Collectors.toList()));
+    }
     public static final double getMedian(final List<Double> DATA) {
         int size = DATA.size();
         Collections.sort(DATA);
         return size % 2 == 0 ? (DATA.get((size / 2) - 1) + DATA.get(size / 2)) / 2.0 : DATA.get(size / 2);
     }
 
+    public static final double getChartDataMin(final List<ChartData> DATA) {
+        return getMin(DATA.stream().map(ChartData::getValue).collect(Collectors.toList()));
+    }
     public static final double getMin(final List<Double> DATA) { return DATA.stream().mapToDouble(v -> v).min().orElse(0); }
 
+    public static final double getChartDataMax(final List<ChartData> DATA) {
+        return getMax(DATA.stream().map(ChartData::getValue).collect(Collectors.toList()));
+    }
     public static final double getMax(final List<Double> DATA) { return DATA.stream().mapToDouble(v -> v).max().orElse(0); }
 }

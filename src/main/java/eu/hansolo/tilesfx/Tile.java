9f5d81bcdd55f8ddfdb096a905a5ae5fed7f24ea
Gerrit Grunwald <han.solo@mac.com>
2021-07-16 10:01:19 +0200

    Cosmetics
    Changes to add/remove series related to issue #121. Thanks to Martin Schelldorfer for the heads up.

--- src/main/java/eu/hansolo/tilesfx/Tile.java
+++ src/main/java/eu/hansolo/tilesfx/Tile.java
@@ -115,7 +115,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.Queue;
 import java.util.concurrent.CopyOnWriteArrayList;
